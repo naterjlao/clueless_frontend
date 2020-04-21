5268b74ba7dae24f1beae8a0d4f77a21b8e1cb07
Andrew Natarian <natarian@umd.edu>
2020-04-20 23:02:37 -0400

    add html/less to update tokens on gameboard with gameState

--- Clue-Less/src/app/gameboard/gameboard.component.ts
+++ Clue-Less/src/app/gameboard/gameboard.component.ts
@@ -23,38 +23,6 @@ export class GameboardComponent implements OnInit {
   ngAfterViewInit() {
   }
 
-  initTokens(){
-    /* Code Backup
-    var players = this.gameState.players;
-    var places = this.gameState.game_board;
-    var characters = JSON.parse(players);    
-    var locations = JSON.parse(places);
-    console.log(players);
-    console.log(places);
-    console.log(characters);
-    console.log(locations);*/
-
-    var places = this.gameState.game_board;
-    var locations = JSON.parse(places);
-
-    /* Map the suspect names in turn_list to an array */
-    var characters = this.gameState.turn_list.map(character => character.suspect);
- 
-    /* Prints array outputs to log for verification */
-    characters.forEach(suspect => console.log(suspect));
-    locations.forEach(tile => console.log(tile));
-
-    /* Loop through each location looking for a player character. */
-    for (let i = 0; i < locations.length; i++){
-      if ('null' != locations[i].suspects){
-        var playPiece = document.getElementById(locations[i]); /* might actually be locations[i].name */
-        playPiece.style.display = "block";
-      }
-    }
-
-
-  }
-
   ngOnDestroy() { //prevent memory leak when component destroyed
     this.gameState_subscription.unsubscribe();
   }
